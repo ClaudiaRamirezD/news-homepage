2783ddb2f56dccab1b663821aae38ff90c54fd99
Claudia Ramirez <clau8812@gmail.com>
2024-10-30 14:37:56 -0600

    Before grid

--- main.js
+++ main.js
@@ -4,20 +4,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelector(".nav-links");
     const closeIcon = document.querySelector(".menu-close");
 
-    //Function to toggle the menu
+    // Function to toggle the menu
     function toggleMenu() {
         backdrop.classList.toggle("open");
         navLinks.classList.toggle("open");
 
         // Toggle the menu icon in the header
         menuIcon.src = backdrop.classList.contains("open")
-        ? "/assets/images/icon-menu-close.svg"
-        : "/assets/images/icon-menu.svg";
+        ? "assets/images/icon-menu-close.svg"
+        : "assets/images/icon-menu.svg"; 
+
+        console.log("Backdrop class: ", backdrop.className);
     }
 
-    // Open menu when clocking the hamburger icon
-    menuIcon.addEventListener("click", toggleMenu);
+    // Open menu when clicking the hamburger icon
+    menuIcon.addEventListener("click", function () {
+        console.log("Menu icon clicked");
+        toggleMenu();
+    });
 
     // Close menu when clicking the close icon inside nav-links
-    closeIcon.addEventListener("click", toggleMenu);
+    closeIcon.addEventListener("click", function () {
+        console.log("Close icon clicked");
+        toggleMenu();
+    });
+
+    // For larger screens nav-links are always visible
+    function handleResize() {
+        if (window.innerWidth >= 60 * 10) {
+        backdrop.classList.remove("open");
+        navLinks.classList.remove("open");
+        menuIcon.src = "assets/images/icon-menu.svg"; 
+        }
+    }
+
+    window.addEventListener("resize", handleResize);
 });
